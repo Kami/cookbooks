152a358b771c4aa387d3bd442f5f09e3276eaac8
Tomaz Muraus <tomaz@tomaz.me>
2011-02-08 02:48:50 +0100

    Remove this line.

--- libmemcached/metadata.rb
+++ libmemcached/metadata.rb
@@ -4,7 +4,6 @@ license          "Apache 2.0"
 description      "Install/Configure libmemcached"
 version          "0.0.1"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-recipe "libmemcached", "Installs libmemcached from source"
 
 supports "ubuntu"
 
